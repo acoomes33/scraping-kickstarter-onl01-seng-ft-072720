1477a695ba6f442de3cc1f3d580bb0211781adf0
Alan Coomes <acoomes1@cord.edu>
2020-08-13 19:38:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   
-  kickstarter.css("li.pro")
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects
 end
 
 create_project_hash
